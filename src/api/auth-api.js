5e9559be5dad84edfa8b8e70e6ab68e2fd2e4e13
Marc Pont <mpont91@gmail.com>
2023-04-15 17:47:30 +0200

    Auth api

---
+++ src/api/auth-api.js
@@ -0,0 +1,8 @@
+import { api } from 'boot/axios'
+
+export const useAuthLoginApi = async (request) =>
+  await api.post('/login', request)
+
+export const useAuthLogoutApi = async () => await api.post('/logout')
+
+export const useAuthUserApi = async () => await api.get('/api/user')
