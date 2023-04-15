0b8800e2b8a6041d1866ea6060cb3feba7e1e981
Marc Pont <mpont91@gmail.com>
2023-04-15 17:23:02 +0200

    Config axios and api urls

--- quasar.config.js
+++ quasar.config.js
@@ -11,7 +11,7 @@
 const { configure } = require('quasar/wrappers')
 const path = require('path')
 
-module.exports = configure(function (/* ctx */) {
+module.exports = configure(function (ctx) {
   return {
     eslint: {
       // fix: true,
@@ -63,7 +63,9 @@ module.exports = configure(function (/* ctx */) {
 
       // publicPath: '/',
       // analyze: true,
-      // env: {},
+      env: {
+        API: ctx.dev ? 'http://localhost' : 'https://api.menumkr.com',
+      },
       // rawDefine: {}
       // ignorePublicFolder: true,
       // minify: false,
